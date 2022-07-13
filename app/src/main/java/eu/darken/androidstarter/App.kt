25f3239ec951039698dbc31c2c8e9dbcb7c626ce
darken <darken@darken.eu>
2022-07-13 15:36:15 +0200

    Refactor to Kotlin Gradle DSL and bump bump bump

--- app/src/main/java/eu/darken/androidstarter/App.kt
+++ app/src/main/java/eu/darken/androidstarter/App.kt
@@ -14,7 +14,10 @@ open class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) Logging.install(LogCatLogger())
+        if (BuildConfig.DEBUG) {
+            Logging.install(LogCatLogger())
+            log(TAG) { "BuildConfig.DEBUG=true" }
+        }
 
         ReLinker
             .log { message -> log(TAG) { "ReLinker: $message" } }
