31f5a3a7cc51b07ca84b1b86f6299ed4bcd1a8f9
darken <darken@darken.eu>
2022-10-27 16:31:56 +0200

    Update release script

--- release.sh
+++ release.sh
@@ -416,8 +416,10 @@ do-push() {
   [yY][eE][sS] | [yY])
     echo -e "\n${S_NOTICE}Pushing files + tags to <${S_NORM}${PUSH_DEST}${S_NOTICE}>..."
     PUSH_MSG=$(git push "${PUSH_DEST}" v"$V_NAME" 2>&1) # Push new tag
-    PUSH_MSG+="\n"
-    PUSH_MSG+=$(git push -u "${PUSH_DEST}" "${REL_PREFIX}${V_NAME}" 2>&1) # Push new tag
+    if [ "$FLAG_NOBRANCH" = false ]; then
+      PUSH_MSG+="\n"
+      PUSH_MSG+=$(git push -u "${PUSH_DEST}" "${REL_PREFIX}${V_NAME}" 2>&1) # Push new tag
+    fi
     if [ ! "$?" -eq 0 ]; then
       echo -e "\n${I_STOP} ${S_WARN}Warning\n$PUSH_MSG"
       # exit 1
